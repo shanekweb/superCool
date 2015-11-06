2e6924edf9b0d44891aeeeec3bcff65de4a79557
Shane Kelly <shanekweb@icloud.com>
2015-11-06 16:36:16 +0000

    pt1 ios course

--- superCool/ViewController.swift
+++ superCool/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var coolLogo: UIImageView!
+    @IBOutlet weak var coolBg: UIImageView!
+    @IBOutlet weak var coolButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func makeItCool(sender: AnyObject) {
+        coolLogo.hidden = false
+        coolBg.hidden = false
+        coolButton.hidden = true
+    }
 
 }
 
